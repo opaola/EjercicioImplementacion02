67dcd64065794d2bec3d5798161672d4b9749899
Andrómeda <128003882+opaola@users.noreply.github.com>
2023-06-01 15:58:01 -0500

    Add files via upload

---
+++ Codigo fuente/streamer.ts
@@ -0,0 +1,19 @@
+class Streamer
+{
+    // Attributes
+	nickname: string;
+	descripcion: string;
+	redessociales: string;
+	stream: Stream;
+	stream: Stream;
+	canal: [Canal];
+	canal: Canal;
+	canal: Canal;
+	
+    // Implemented attributes
+	
+    // Operations
+	 
+	// Implemented operations
+	
+}
